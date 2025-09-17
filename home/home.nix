d06bd801f3d31fce2a239af53f7f8883a170f146
Rorikstr | Rust Dev <slava.070299@gmail.com>
2025-09-17 21:22:29 +0300

    finally

---
+++ home/home.nix
@@ -0,0 +1,114 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "wave";
+  home.homeDirectory = "/home/wave";
+  home.stateVersion = "25.05";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    fd
+    ripgrep
+    zoxide
+    eza
+    bat
+    procs
+    dust
+    duf
+    helix
+    starship
+    atuin
+  ];
+
+  home.sessionVariables = {
+    BAT_STYLE = "plain";
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        llvm-vs-code-extensions.vscode-clangd
+        mhutchie.git-graph
+        # robbowen.synthwave-vscode
+        rust-lang.rust-analyzer
+        vadimcn.vscode-lldb
+        formulahendry.code-runner
+        esbenp.prettier-vscode
+      ];
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = ''
+      eval "$(zoxide init bash)"
+    '';
+    shellAliases = {
+      hxroot = "sudo hx /etc/nixos/configuration.nix";
+      nxr = "sudo nixos-rebuild switch";
+      hxhome = "hx ${config.home.homeDirectory}/nix-config/home/home.nix";
+      nxh = "home-manager switch --flake ${config.home.homeDirectory}/nix-config/home";
+      
+      find = "fd";
+      fd = "fd";
+      
+      grep = "rg";
+      rg = "rg";
+      
+      cd = "z";
+      z = "zoxide";
+      
+      ls = "eza";                    
+      ll = "eza -la";                
+      lt = "eza --tree";             
+      cat = "bat";                   
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    settings = {
+      # Настройка директории для показа полного пути
+      directory = {
+        truncation_length = 0;  # 0 = показывать полный путь
+        truncate_to_repo = false;
+        style = "green";
+      };
+      
+      # Остальные настройки
+      format = "$all";
+      character = {
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold red)";
+      };
+    };
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableBashIntegration = true;
+    
+    settings = {
+      dialect = "us";
+      style = "compact";  # стиль отображения результатов
+      inline_height = 20;  # высота inline окна
+      
+      # Настройки синхронизации (опционально)
+      # sync_address = "https://api.atuin.sh";
+      # sync_frequency = "10m";  # частота синхронизации
+      
+      # Поведение
+      enter_accept = true;  # Enter выбирает команду
+      fuzzy = true;         # нечеткий поиск
+      history_filter = [
+        "secret*"
+        "*password*"
+      ];  # фильтрация чувствительных команд
+    };
+  };
+}
---
+++ hosts/nxos/configuration.nix
@@ -0,0 +1,375 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nxos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Генерация нужных локалей
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "ru_RU.UTF-8/UTF-8"
+  ];
+
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true;
+  };
+
+  # Настройка раскладки для X11 и TTY
+  services.xserver.xkb = {
+    layout = "us,ru"; # us — первая, значит по умолчанию
+    options = "grp:alt_shift_toggle"; # переключение по Alt+Shift
+  };
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  services.xserver.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    # WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+    # GLFW_BACKEND = "wayland"; # Явное указание бэкенда для Kitty
+  };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+  ];
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # Podman
+  virtualisation.podman.enable = true;
+
+  # Printing
+  services.printing.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.wave = {
+    isNormalUser = true;
+    home = "/home/wave";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "docker"
+      "podman"
+      "libvirtd"
+      "audio"
+      "video"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    # KDE
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kclock # Clock app
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    # kdiff3 # Compares and merges 2 or 3 files or directories
+    # kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    # kdePackages.partitionmanager # Optional: Manage the disk devices, partitions and file systems on your computer
+    kdePackages.kate
+    kdePackages.konsole
+    kdePackages.dolphin
+    kdePackages.ark
+    kdePackages.gwenview
+    kdePackages.spectacle
+    # kdePackages.kamoso
+    # Non-KDE graphical packages
+    hardinfo2 # System information and benchmarks for Linux systems
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
+
+    home-manager
+    # Браузеры
+    firefox
+    chromium
+    librewolf
+
+    # Редакторы
+    mandoc
+    vim
+    nano
+    helix
+
+    # Мультимедиа
+    vlc
+    # gimp
+    # blender
+    audacity
+    # lmms # DAW similar to FL Studio (music production software)
+    obs-studio
+
+    # Офис
+    libreoffice-qt6-fresh
+
+    # Utils
+    atuin
+    fzf
+    eza
+    bat
+    procs
+    dust
+    duf
+    starship
+    ripgrep
+    fd # Simple, fast and user-friendly alternative to find
+    p7zip
+    mkvtoolnix-cli
+    neofetch
+    htop
+    btop
+    git
+    wget
+    curl
+    tree
+    zip
+    unzip
+    # fzf # Command-line fuzzy finder written in Go
+    zoxide
+    qalculate-qt
+    jq
+    bc
+    less
+    btrfs-progs
+
+    # shell terminal
+    nushell
+    zsh
+
+    keepassxc
+
+    telegram-desktop
+    qbittorrent
+
+    tor-browser
+    rocketchat-desktop
+    hiddify-app
+
+    # develop
+    clang-tools
+    nodejs_24
+    vscode
+    gnumake
+    rustup
+    cargo
+    rustc
+    # go
+    postgresql
+    cmake
+    gcc
+    gdb
+    valgrind
+    doxygen
+    lcov
+    cppcheck
+    check
+    # maven
+    # jdk17
+    python3
+    python3Packages.pip
+    gcovr
+    python3Packages.mutagen
+
+    # Система
+    # ipset
+    # nftables
+    # ipcalc
+    docker
+    # podman
+    # podman-compose
+    # virt-manager
+    # qemu
+    dnsmasq
+    iwd # Wireless daemon for Linux
+    openvpn
+    nmap
+    # stress-ng # Stress test a computer system
+
+    # Аудио/видео
+    pipewire
+    wireplumber
+    vivaldi
+
+    # Другое
+    # luakit # Fast, small, webkit-based browser framework extensible in Lua
+    # msr-tools # Tool to read/write from/to MSR CPU registers on Linux
+    # powertop # Analyze power consumption on Intel-based laptops
+    smartmontools # Tools for monitoring the health of hard drives
+    yt-dlp
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network   Game Transfers
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
